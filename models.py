4874aca885eb83408bb1473060eab72c1366cef4
Margaux Faurie <74653873+Margaux-F@users.noreply.github.com>
2020-11-26 21:01:20 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-# UTF-8
-# Author : Margaux Faurie
-# Date : 19 Nov. 2020
+# UTF8
+# Date: 26 Nov. 2020
+# Author: Margaux Faurie
 
 from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from sqlalchemy import DateTime, Boolean
@@ -12,12 +12,13 @@ session = connect('aam_test_301')
 Base = declarative_base()
 
 
+
 class books(Base):
     """docstring for """
     __tablename__ = "books"
     id = Column(Integer, primary_key=True, nullable = False)
     Title = Column(String(20), nullable = True)
-    Author = Column(String(20))
+    Author = Column(String(20), ForeignKey("authors.id"), nullable = True)
 
 class authors(Base):
     """docstring for """
@@ -27,11 +28,10 @@ class authors(Base):
     Last_Name = Column(String(20), nullable = True)
     Date_Birth = Column(String(20), nullable = True)
     Date_Death = Column(String(10), nullable = True)
-    Nationality = Column(String(5))
+    Nationality = Column(String(5), ForeignKey("nation.Name"), nullable = True)
 
 class nation(Base):
     """docstring for"""
     __tablename__ = "nation"
     id = Column(Integer, primary_key = True)
-    country = Column(String(5), nullable = True)
-
+    Name = Column(String(5), nullable = True)
