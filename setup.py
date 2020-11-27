13470c8e61d1831e6771adeb90141b149c80e4e9
Margaux Faurie <74653873+Margaux-F@users.noreply.github.com>
2020-11-27 14:56:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
 # UTF-8
 # Author : Margaux Faurie
-# Date : 26 Nov. 2020
+# Date : 26 Nov. 2020 14h56
 
-import json
-from sqlalchemy.orm import sessionmaker, query
-from connection import connect, createdb, checkdb
-from datetime import datetime
-from createtables import createtables
-from populate import populate
-import sqlalchemy as db
-from models import books
-import pandas as pd
 
 #-----------------------------
 # Create the database --> C
@@ -33,65 +24,95 @@ with open("config.json") as f: #Load data for the configuration
         host = config["host"]
         port = config["port"]
 
+def Checkdb(dbname):
+    prgrm = 1
+    if checkdb(dbname) is True: #Check the existance of the database (if exists or not)
+        createdb(dbname) #Create the database
+        session = connect(dbname) #Connect to the database
 
-if checkdb(dbname) is True: #Check the existance of the database (if exists or not)
-    createdb(dbname) #Create the database
-    session = connect(dbname) #Connect to the database
+        createtables(dbname) #Create the tables
+        populate(dbname, rawdata) #Populate the database
 
-    createtables(dbname) #Create the tables
-    populate(dbname, rawdata) #Populate the database
+        finishTime = datetime.now() #End of the set up
+        timeDetla = finishTime - startTime
 
-    finishTime = datetime.now() #End of the set up
-    timeDetla = finishTime - startTime
+        print('\n----------------------------------------------------')
+        print("Setup is finished. Your database is now available.")
+        print("The process was completed in : " + str(
+            timeDetla.total_seconds()) + "s.") #Total time
+        print('----------------------------------------------------\n')
+        return prgrm
+    else:
+        print("Your database already exists.")
+        print('Do you want to delete the tables to run the program anyway ?')
+        print('1 = Delete the tables and recreate others.\n')
+        
+        try:
+            Choice = int(input('Your choice : '))
+        except ValueError:
+            print("The input is not right...")
+            prgrm = 0
+            return prgrm
 
-    print('\n----------------------------------------------------')
-    print("Setup is finished. Your database is now available.")
-    print("The process was completed in : " + str(
-        timeDetla.total_seconds()) + "s.") #Total time
-    print('----------------------------------------------------\n')
+        if Choice == 1:
+            engine = db.create_engine(f'mysql+pymysql://{username}:{password}@{host}/{dbname}')
+            connection = engine.connect() #Connect
 
+            session = connect(dbname) #Connect to the database
+
+            metadata = db.MetaData()
+
+            metadata.drop_all(engine)
+            
+            createtables(dbname) #Create the tables
+            populate(dbname, rawdata) #Populate the database
+            return prgrm
+        else : 
+            prgrm = 0
+            print('The program will not run')
+            return prgrm
+
+        print('----------------------------------------------------\n')
+
+prgrm = 1 
+prgrm = Checkdb(dbname)
+if prgrm == 1:
+    # ---------------------------------
+    # How to read a database ?  --> R
+    # ---------------------------------
+
+    engine = db.create_engine(f'mysql+pymysql://{username}:{password}@{host}/{dbname}')
+    connection = engine.connect() #Reconnect to the database
+
+    def printtable(connection): # Def a function that print a database using pandas
+        return pd.DataFrame(connection.execute("SELECT * FROM books"))
+
+    print("Original database:\n")
+    print(printtable(connection))
+    print('\n----------------------------------------------------\n')
+
+
+    # ---------------------------
+    # Update book  --> U
+    # ---------------------------
+
+    query = db.update(books).values(Author=4).where(books.Title=="The World as I See It")
+    results = connection.execute(query)
+
+    print("Database with update:\n")
+    print(printtable(connection))
+    print('\n----------------------------------------------------\n')
+
+
+    # ------------------------------------------------------
+    # Suppresion du livre lu "The World as I See It" --> D
+    # ------------------------------------------------------
+
+    query = db.delete(books).where(books.Title == "The World as I See It")
+    results = connection.execute(query)
+
+    print("Database with deleted element:\n")
+    print(printtable(connection))
+    print('\n----------------------------------------------------\n')
 else:
-    print("Your database already exists.")
-    print("Please drop it and retry.")
-    print('----------------------------------------------------\n')
-
-
-# ---------------------------------
-#How to read a database ?  --> R
-# ---------------------------------
-
-engine = db.create_engine(f'mysql+pymysql://{username}:{password}@{host}/{dbname}')
-connection = engine.connect() #Reconnect to the database
-
-def printtable(connection): #Print a database using pandas
-    return pd.DataFrame(connection.execute("SELECT * FROM books"))
-
-print("Original database:\n")
-print(printtable(connection))
-print('\n----------------------------------------------------\n')
-
-
-# ---------------------------
-#Update book  --> U
-# ---------------------------
-
-    
-query = db.update(books).values(Author=4).where(
-        books.Title=="The World as I See It")
-results = connection.execute(query)
-
-print("Database with update:\n")
-print(printtable(connection))
-print('\n----------------------------------------------------\n')
-
-
-# ------------------------------------------------------
-#Suppresion du livre lu "The World as I See It" --> D
-# ------------------------------------------------------
-
-query = db.delete(books).where(books.Title == "The World as I See It")
-results = connection.execute(query)
-
-print("Database with deleted element:\n")
-print(printtable(connection))
-print('\n----------------------------------------------------\n')
+    print('You asked not to run the program')
